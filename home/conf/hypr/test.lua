aa3b833f2194e5bb4fc067c77564fdba55247cb7
Christian <pap3redits@gmail.com>
2026-05-21 15:33:09 -0400

    start of converting to lua

--- home/conf/hypr/test.lua
+++ home/conf/hypr/test.lua
@@ -2,10 +2,130 @@
 -- Imports
 require("~/.config/hypr/rose-pine.conf")
 
-
+-- Monitor setup
 hl.monitor({
   output = "",
   mode = "preferred",
   position = "auto"
   scale = "auto",
+})
+hl.monitor({
+  output = "DP-10",
+  mode = "preferred",
+  position = "auto",
+  scale = "auto"
+  transform = "1"
+}
+
+-- Program variables
+local terminal = "ghostty -e nu"
+local fileManager = terminal "-e yazi"
+local menu = "walker"
+local browser = "flatpak run app.zen_browser.zen"
+
+-- Autostart
+hl.on("hyprland.start", function ()
+  hl.exec_cmd("wayle panel start")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+  hl.exec_cmd("walker --gapplication-service &")
+  hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("elephant &")
+  hl.exec_cmd("clipse -listen")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme "orchis-dark"")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme "${config.home.pointerCursor.name}"")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size ${toString home.pointerCursor.size}")
+end)
+
+-- Environment variables
+hl.env("QT_QPA_PLATFORMTHEME,qt6ct")
+hl.env("AQ_DRM_DEVICES,/dev/dri/card0:/dev/dri/card1") -- not sure if still needed
+
+-- General window appearance
+hl.config({
+  general = {
+    gaps_in = 5,
+    gaps_out = 20,
+
+    border_size = 2,
+
+    col = {
+      active_border = rose
+      inactive_border = muted
+    },
+
+    resize_on_border = false,
+
+    allow_tearing = false,
+
+    layout = "dwindle",
+  },
+
+  decoration = {
+    rounding = 8,
+
+    active_opacity = 1.0,
+    inactive_opacity = 1.0,
+
+    shadow = {
+      enabled = true,
+      range = 4,
+      render_power = 3,
+      color = base,
+    },
+
+    blur = {
+      enabled = true,
+      size = 3,
+      passes = 1,
+      vibrancy = 0.1696,
+    },
+  },
+
+  animations = {
+    enabled = true,
+  },
+})
+
+-- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
+hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+
+-- Default springs
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+
+hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
+hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
+hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+
+hl.config({
+  dwindle = {
+    pseudotile = true,
+    preserve_split = true,
+  },
+
+  master = {
+    new_status = "master"
+  },
+
+  scrolling = {
+    full
+  },
 })
