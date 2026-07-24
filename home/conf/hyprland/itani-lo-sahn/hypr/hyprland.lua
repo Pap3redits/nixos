0c9a939e1c03829c83a53985a41e278f1100183b
christian <pap3redits@gmail.com>
2026-07-24 10:05:38 -0400

    add laptop hyprland config

---
+++ home/conf/hyprland/itani-lo-sahn/hypr/hyprland.lua
@@ -0,0 +1,157 @@
+
+-- Imports
+require("rose-pine")
+require("keybinds")
+
+-- Monitor setup
+hl.monitor({
+  output = "",
+  mode = "preferred",
+  position = "auto",
+  scale = "1",
+  bitdepth = 8,
+})
+hl.monitor({
+  output = "DP-10",
+  mode = "preferred",
+  position = "auto",
+  scale = "auto",
+  transform = 1,
+  bitdepth = 8,
+})
+
+-- Program variables
+
+
+-- Autostart
+hl.on("hyprland.start", function ()
+  hl.exec_cmd("noctalia")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent")
+  -- hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("clipse -listen")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'orchis-dark'")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme '${config.home.pointerCursor.name}'")
+  hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-size ${toString home.pointerCursor.size}")
+end)
+
+-- Environment variables
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("AQ_DRM_DEVICES", "/dev/dri/card0:/dev/dri/card1") -- not sure if still needed
+
+-- General window appearance
+hl.config({
+  general = {
+    gaps_in = 10,
+    gaps_out = 10,
+
+    border_size = 2,
+
+    col = {
+      active_border = rose,
+      inactive_border = muted
+    },
+
+    resize_on_border = false,
+
+    allow_tearing = false,
+
+    layout = "dwindle",
+  },
+
+  decoration = {
+    rounding = 8,
+
+    active_opacity = 1.0,
+    inactive_opacity = 1.0,
+
+    shadow = {
+      enabled = true,
+      range = 4,
+      render_power = 3,
+      color = base,
+    },
+
+    blur = {
+      enabled = true,
+      size = 3,
+      passes = 1,
+      vibrancy = 0.1696,
+    },
+
+  },
+
+  animations = {
+    enabled = true,
+  },
+})
+
+-- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
+hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
+hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
+hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
+hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
+hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+
+-- Default springs
+hl.curve("easy",           { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
+
+hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
+hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
+hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
+hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
+hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
+hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
+hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
+hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
+hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
+hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
+hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
+hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
+hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+
+hl.config({
+  dwindle = {
+    preserve_split = true,
+  },
+
+  master = {
+    new_status = "master",
+  },
+
+  scrolling = {
+    fullscreen_on_one_column = true,
+  },
+
+  misc = {
+    force_default_wallpaper = 0,
+    disable_hyprland_logo = true,
+  },
+
+  input = {
+    kb_layout = "us",
+    kb_variant = "",
+    kb_model   = "",
+    kb_options = "",
+    kb_rules   = "",
+    numlock_by_default = true,
+    follow_mouse = 1,
+
+    sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+    touchpad = {
+        natural_scroll = true,
+    },
+  },
+ 
+
+})
+hl.device({
+    name        = "epic-mouse-v1",
+    sensitivity = -0.5,
+})
+
---
+++ home/ui/hyprland-itani-lo-sahn.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, lib, osConfig, ... }:
+lib.mkIf (osConfig.networking.hostName == "itani-lo-sahn")
+{
+
+
+  #gtk.gtk4.theme = null;
+
+  #wayland.windowManager.hyprland = {
+  #  enable = true;
+  #  package = null;
+  #  portalPackage = null;
+  #};
+
+  xdg.configFile."hypr" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/home/conf/hyprland/itani-lo-sahn/hypr";
+    force = true;
+    recursive = true;
+  };
+
+  programs.hyprlock.enable = true;
+  stylix.targets.hyprlock.enable = false;
+  /*
+    services.hyprpaper = {
+      enable = true;
+      settings.splash = false;
+      settings.wallpaper = [
+        {
+          monitor = "";
+          path = "/etc/nixos/assets/wallpapers/polyscape_pillars_2.png";
+
+        }
+      ];
+    };
+  */
+
+}
