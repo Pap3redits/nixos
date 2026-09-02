3beb486f38d10d7d5ad041a30d6f1e9fdb0b5821
Christian <paperedits2@pm.me>
2026-09-01 20:01:21 -0400

    zipline keys

--- home/conf/hyprland/ira-domini/hypr/keybinds.lua
+++ home/conf/hyprland/ira-domini/hypr/keybinds.lua
@@ -26,8 +26,8 @@ hl.bind(mainMod .. " + O", hl.dsp.workspace.toggle_special("obsidian"))
 hl.bind(mainMod .. " + N", hl.dsp.workspace.toggle_special("note"))
 
 --Screenshots
-hl.bind("PRINT", hl.dsp.exec_cmd("hyprshot -m active"))
 hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
+hl.bind("PRINT", hl.dsp.exec_cmd("hyprshot -o ~/Pictures -m active -m output -- ~/bin/zipline.sh"))
 
 --Window resizing
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
--- home/conf/hyprland/itani-lo-sahn/hypr/keybinds.lua
+++ home/conf/hyprland/itani-lo-sahn/hypr/keybinds.lua
@@ -27,8 +27,8 @@ hl.bind(mainMod .. " + O", hl.dsp.workspace.toggle_special("obsidian"))
 hl.bind(mainMod .. " + N", hl.dsp.workspace.toggle_special("note"))
 
 --Screenshots
-hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m active ~/bin/zipline.sh"))
---hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
+hl.bind(mainMod .. " + PRINT", hl.dsp.exec_cmd("hyprshot -m region --clipboard-only"))
+hl.bind("PRINT", hl.dsp.exec_cmd("hyprshot -o ~/Pictures -m active -m output -- ~/bin/zipline.sh"))
 
 --Window resizing
 hl.bind(mainMod .. " + mouse:272", hl.dsp.window.drag(),   { mouse = true })
